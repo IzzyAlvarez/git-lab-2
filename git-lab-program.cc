2db4ef3df83c1d77e7d34959b15bfb5937ee3088
Isabel Alvarez <ia638719@ohio.edu>
2020-08-25 18:26:58 -0400

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Isabel Alvarez
+ *        Date: 8/25/20
+ * Description: Just a little program :)
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
